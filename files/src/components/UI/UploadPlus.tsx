dbf7063306243a75c65006f75a337b7e182fc73b
tolu-c <tolu.adeyemo7@gmail.com>
2023-05-25 18:54:22 +0100

    fix: broken url

--- files/src/components/UI/UploadPlus.tsx
+++ files/src/components/UI/UploadPlus.tsx
@@ -6,7 +6,7 @@ export const UploadPlus = () => {
   return (
     <span
       className="absolute bottom-8 right-8 w-12 md:w-16 h-12 md:h-16 rounded-full flex items-center justify-center bg-slate-800 hover:bg-slate-950 text-slate-100 z-40 cursor-pointer"
-      onClick={() => navigate("/upload")}
+      onClick={() => navigate("upload")}
     >
       <PlusIcon className="w-6 md:w-8 h-6 md:h-8" />
     </span>
